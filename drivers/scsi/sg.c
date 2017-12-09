705a61012206506be9b8c426a72bf1f9aa7f98af
Adek Maulana <adekm@techdro.id>
2017-12-09 20:55:49 +0700

    scsi: fix derp after tag 3.18.72
    Change-Id: I3467a28b2d9eb8d08beb8b29a9e030268ee44804
    Signed-off-by: Adek Maulana <adekm@techdro.id>

--- drivers/scsi/sg.c
+++ drivers/scsi/sg.c
@@ -861,32 +861,30 @@ sg_fill_request_table(Sg_fd *sfp, sg_req_info_t *rinfo)
 	int val;
 	unsigned int ms;
 
-	for (srp = sfp->headrp, val = 0; val < SG_MAX_QUEUE;
-	     ++val, srp = srp ? srp->nextrp : srp) {
-	     memset(&rinfo[val], 0, SZ_SG_REQ_INFO);
-	if (srp) {
-	rinfo[val].req_state = srp->done + 1;
+	val = 0;
+	list_for_each_entry(srp, &sfp->rq_list, entry) {
+		if (val > SG_MAX_QUEUE)
+			break;
+		memset(&rinfo[val], 0, SZ_SG_REQ_INFO);
+		rinfo[val].req_state = srp->done + 1;
 		rinfo[val].problem =
-		     srp->header.masked_status &
-		     srp->header.host_status &
-		     srp->header.driver_status;
-			if (srp->done)
-				rinfo[val].duration =
+			srp->header.masked_status &
+			srp->header.host_status &
+			srp->header.driver_status;
+		if (srp->done)
+			rinfo[val].duration =
 				srp->header.duration;
-			else {
-				ms = jiffies_to_msecs(jiffies);
-				rinfo[val].duration =
-				     (ms > srp->header.duration) ?
-				     (ms - srp->header.duration) : 0;
-			}
-			rinfo[val].orphan = srp->orphan;
-			rinfo[val].sg_io_owned =
-					srp->sg_io_owned;
-			rinfo[val].pack_id =
-					srp->header.pack_id;
-			rinfo[val].usr_ptr =
-					srp->header.usr_ptr;
+		else {
+			ms = jiffies_to_msecs(jiffies);
+			rinfo[val].duration =
+				(ms > srp->header.duration) ?
+				(ms - srp->header.duration) : 0;
 		}
+		rinfo[val].orphan = srp->orphan;
+		rinfo[val].sg_io_owned = srp->sg_io_owned;
+		rinfo[val].pack_id = srp->header.pack_id;
+		rinfo[val].usr_ptr = srp->header.usr_ptr;
+		val++;
 	}
 }
 
