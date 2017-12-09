99e75824161c21e3e8293114a9134b5213c919bc
Adek Maulana <adekm@techdro.id>
2017-12-10 01:15:30 +0700

    cpufreq: impulse improvement tuning
    Change-Id: I8ff33473303cc6e85bc8c4c8fcb511b013167ded
    Signed-off-by: Adek Maulana <adekm@techdro.id>

--- drivers/cpufreq/cpufreq_impulse.c
+++ drivers/cpufreq/cpufreq_impulse.c
@@ -72,7 +72,7 @@ static unsigned int default_target_loads[] = {DEFAULT_TARGET_LOAD};
 
 #define DEFAULT_TIMER_RATE (20 * USEC_PER_MSEC)
 #define DEFAULT_TIMER_RATE_SUSP ((unsigned long)(50 * USEC_PER_MSEC))
-#define DEFAULT_ABOVE_HISPEED_DELAY DEFAULT_TIMER_RATE
+#define DEFAULT_ABOVE_HISPEED_DELAY 40000
 static unsigned int default_above_hispeed_delay[] = {
 	DEFAULT_ABOVE_HISPEED_DELAY };
 
@@ -454,7 +454,7 @@ static void cpufreq_impulse_timer(unsigned long data)
 #ifdef CONFIG_STATE_NOTIFIER
 	tunables->boosted = tunables->boosted && !state_suspended;
 #endif
-	this_hispeed_freq = max(tunables->hispeed_freq, ppol->policy->min);
+	this_hispeed_freq = min(tunables->hispeed_freq, ppol->policy->min);
 
 	if (tunables->boosted) {
 		if (ppol->target_freq < this_hispeed_freq &&
@@ -1253,7 +1253,7 @@ static int cpufreq_governor_impulse(struct cpufreq_policy *policy,
 
 		freq_table = cpufreq_frequency_get_table(policy->cpu);
 		if (!tunables->hispeed_freq)
-			tunables->hispeed_freq = policy->max;
+			tunables->hispeed_freq = policy->min;
 
 		ppol = per_cpu(polinfo, policy->cpu);
 		ppol->policy = policy;
